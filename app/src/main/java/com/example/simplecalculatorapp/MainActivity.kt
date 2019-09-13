87a3c688c62b794b08254ea85ec07bbc68b4c129
MrMalith <sachinthamalith047@gmail.com>
2019-09-13 21:43:24 +0530

    methods implemented to abstract two inputs

--- app/src/main/java/com/example/simplecalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculatorapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +17,15 @@ class MainActivity : AppCompatActivity() {
         val firstNum = findViewById<EditText>(R.id.firstNumEditText)
         val secNum = findViewById<EditText>(R.id.secondNumEditText)
         val addBtn = findViewById<Button>(R.id.addButton)
+        val absBtn =findViewById<Button>(R.id.absButton)
         val resultTxtView = findViewById<TextView>(R.id.resultTextView)
 
         addBtn.setOnClickListener {
             resultTxtView.text = "Addition is "+(firstNum.text.toString().toInt()+secNum.text.toString().toInt()).toString()
+            Toast.makeText(this,resultTxtView.text, Toast.LENGTH_LONG).show()
+        }
+        absBtn.setOnClickListener {
+            resultTxtView.text = "Abstraction is "+(firstNum.text.toString().toInt()-secNum.text.toString().toInt()).toString()
             Toast.makeText(this,resultTxtView.text, Toast.LENGTH_LONG).show()
         }
     }
